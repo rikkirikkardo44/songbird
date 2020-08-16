97e5b786aff79a8e7038f61b96e392db33a4344d
rikkirikkardo44 <en.unger@gmail.com>
2020-08-17 02:21:30 +0500

    feat: add logic and style  currentQuestionPanel

--- src/components/currentQuestionPanel/currentQuestionPanel.js
+++ src/components/currentQuestionPanel/currentQuestionPanel.js
@@ -1,11 +1,29 @@
 import React from "react";
 
+import imageB4Play from '../../files/grafitti.jpg';
+import AudioPlayer from '../player/player';
 import './currentQuestionPanel.css';
 
-const CurrentQuestionPanel = () => {
-    return (
-        <div>
+const CurrentQuestionPanel = (props) => {
+    const {audio, img, name} = props.data;
 
+    return (
+        <div className="current-question-panel jumbotron rounded">
+            {props.levelWin ? <img className="bird-image" src={img} alt={name}/> :
+                <img className="bird-image" src={imageB4Play} alt="image"/>}
+            <div className="label-audio">
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">
+                        <h3>{props.levelWin ? name : '**********'}</h3>
+                    </li>
+                </ul>
+                <div className="audio">
+                    <AudioPlayer src={audio}/>
+                </div>
+
+            </div>
         </div>
-    )
-}
+    );
+};
+
+export default CurrentQuestionPanel;
