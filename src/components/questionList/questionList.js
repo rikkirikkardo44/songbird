a9c6c9b041b088f4a1d98d40fa8223b168806804
rikkirikkardo44 <en.unger@gmail.com>
2020-08-24 18:55:31 +0500

    feat: QuestionList style update

--- src/components/questionList/questionList.js
+++ src/components/questionList/questionList.js
@@ -2,16 +2,15 @@ import React from "react";
 
 import './questionList.css';
 
-const QuestionList = (props) => {
+const QuestionList = ({page}) => {
     const labels = [
         {name: 'Разминка', index: 0},
-        {name: 'Воробьиные', index: 1},
-        {name: 'Лесные птицы', index: 2},
-        {name: 'Певчие птицы', index: 3},
-        {name: 'Хищные птицы', index: 4},
+        {name: 'Домашние птицы', index: 1},
+        {name: 'Ночные птицы', index: 2},
+        {name: 'Нелетающие птицы', index: 3},
+        {name: 'Дикие птицы', index: 4},
         {name: 'Морские птицы', index: 5}
     ];
-    const {page} = props;
     const liElements = labels.map(({name, index}) => {
         const active = index === page ? "page-item active" : "page-item disabled";
         return <li className={active} key={index}><a className="page-link" href="/#">{name}</a></li>
